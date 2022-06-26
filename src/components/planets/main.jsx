a52da7732ff3a01d6185d16beb2ead7fb4d259c4
Genesio S Pacehco <genesio.s.p.28@gmail.com>
2022-06-26 11:04:09 -0300

    Adding const satNames planet/main.jsx(L.6). Change the grayimg to recive true or false from planet.

--- src/components/planets/main.jsx
+++ src/components/planets/main.jsx
@@ -11,9 +11,18 @@ const Planets = () => {
       <ul>
           <Planet
             name="Mercúrio"
-            img_url="https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg"
-            link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
             description="Mercúrio é o menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar."
+            link="https://pt.wikipedia.org/wiki/Merc%C3%BArio_(planeta)"
+            img_url="https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg"
+            gray={false}
+            clickOnPlanet={clickOnPlanet}
+          />
+          <Planet
+            name="Vênus"
+            description="Vénus (português europeu) ou Vênus (português brasileiro) é o segundo planeta do Sistema Solar em ordem de distância a partir do Sol, orbitando-o a cada 224,7 dias."
+            link="https://pt.wikipedia.org/wiki/V%C3%A9nus_(planeta)"
+            img_url="https://upload.wikimedia.org/wikipedia/commons/8/85/Venus_globe.jpg"
+            gray={true}
             clickOnPlanet={clickOnPlanet}
           />
       </ul>
--- src/components/planets/planet/main.jsx
+++ src/components/planets/planet/main.jsx
@@ -3,11 +3,20 @@ import GrayImg from "../../shared/GrayImg/main"
 
 
 const Planet = (props) => {
+  const satNames = ['sat1', 'sat2', 'sat3', 'sat4']
+  const satellites = satNames.map((satName) => {
+    return <li>{satName}</li>
+  })
+
   return (
     <li onClick={() => props.clickOnPlanet(props.name)}>
       <h4>{props.name}</h4>
       <DescriptionWithLink link={props.link} description={props.description} />
-      <GrayImg img_url={props.img_url} gray={true} />
+      <GrayImg img_url={props.img_url} gray={props.gray} />
+      <ul>
+        {satellites}
+      </ul>
+      <br />
     </li>
   )
 }
